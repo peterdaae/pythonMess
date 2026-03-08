e5b2056c4571d58fa197272503df9b15963a1a6e
Peter Daae <peterhorndaae@gmail.com>
2026-03-09 00:18:13 +0100

    continued work on lab5

--- Lecture5/exercise1.py
+++ Lecture5/exercise1.py
@@ -2,6 +2,7 @@ class Exercise1:
     def _reverse_lookup(self):
         person = {"name": "Alice", "age": 30, "city": "Oslo"}
         address = {"City": "Norway", "Postcode": 1042, "Street address": "Lampvei 2"}
+        address = {"City": "Sweden", "Postcode": 13001, "Street address": "ojqåkrq 2"}
 
 
     def run_operations(self):
